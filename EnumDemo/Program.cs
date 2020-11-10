8742a673d54ce0db3d817aff80133b3f625605b5
JARNO MAES <JARNO.MAES@student.intecbrussel.be>
2020-11-10 09:54:31 +0100

    Add project files.

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    enum workDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //workDays d = (workDays);
+            //Console.WriteLine(d);
+
+            //workDays d = workDays.Monday;
+            //Console.WriteLine(d);
+
+            
+            
+            //Enumarate values 0,1,2,..
+            foreach (int i in Enum.GetValues(typeof(workDays)))
+            {
+                Console.WriteLine(i);
+            }
+            
+            //Enumarate Names  Monday,Tuesday,...
+            foreach (string s in Enum.GetNames(typeof(workDays)))
+            {
+                Console.WriteLine(s);
+            }
+
+
+
+        }
+    }
+}
