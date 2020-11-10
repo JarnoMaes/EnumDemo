2b06fb08dd17fe34656b00893809ed9c7162ad7d
JARNO MAES <JARNO.MAES@student.intecbrussel.be>
2020-11-10 10:06:22 +0100

    Edit

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EnumDemo
 {
-    enum workDays
+    enum Days
     {
         Monday,
         Tuesday,
@@ -18,28 +19,34 @@ namespace EnumDemo
     {
         static void Main(string[] args)
         {
-           
-            //workDays d = (workDays);
-            //Console.WriteLine(d);
+            #region EnumDemo
+            ////workDays d = (workDays);
+            ////Console.WriteLine(d);
 
-            //workDays d = workDays.Monday;
-            //Console.WriteLine(d);
-
-            
-            
-            //Enumarate values 0,1,2,..
-            foreach (int i in Enum.GetValues(typeof(workDays)))
-            {
-                Console.WriteLine(i);
-            }
-            
-            //Enumarate Names  Monday,Tuesday,...
-            foreach (string s in Enum.GetNames(typeof(workDays)))
-            {
-                Console.WriteLine(s);
-            }
+            ////workDays d = workDays.Monday;
+            ////Console.WriteLine(d);
 
 
+
+            ////Enumarate values 0,1,2,..
+            //foreach (int i in Enum.GetValues(typeof(workDays)))
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            ////Enumarate Names  Monday,Tuesday,...
+            //foreach (string s in Enum.GetNames(typeof(workDays)))
+            //{
+            //    Console.WriteLine(s);
+            //}
+            #endregion
+
+
+            //Console.WriteLine("Select a day please: ");
+            //Days d = Days.Tuesday;
+
+            //int day = (int)d;
+            //Console.WriteLine(day);
 
         }
     }
